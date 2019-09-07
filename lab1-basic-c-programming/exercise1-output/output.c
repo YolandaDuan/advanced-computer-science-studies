695a97b2683605d9291a8d4ce0bb642af91ba844
yolanda <dzm13546157880@gmail.com>
2019-09-07 08:20:51 +0200

    first try to submit code

--- lab1-basic-c-programming/exercise1-output/output.c
+++ lab1-basic-c-programming/exercise1-output/output.c
@@ -1,1 +1,12 @@
-hello world.
+#include <stdio.h>
+
+int main (int argc, char ** argv) {
+    int a = 10, b = 3, c;
+    
+    
+    printf("One half is 50 %% \n");
+    printf("The difference between 10 and 3 is 7 \n");
+    printf("1.000000 / 3.000000 is 0.333333 \n");
+
+    return 0;
+}
