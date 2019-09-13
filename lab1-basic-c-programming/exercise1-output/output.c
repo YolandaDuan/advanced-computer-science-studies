efb770c6f66927535d55138e1c38dccbd8b46b4e
yolanda <dzm13546157880@gmail.com>
2019-09-13 13:34:14 +0200

    rewrite exercise 1 output.c

--- lab1-basic-c-programming/exercise1-output/output.c
+++ lab1-basic-c-programming/exercise1-output/output.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main (int argc, char ** argv) {
-  //  int a = 10, b = 3, c;
-
+    int a = 10, b = 3;
+    float f1 = 1, f2 = 3;
     
     printf("One half is 50 %% \n");
-    printf("The difference between 10 and 3 is 7 \n");
-    printf("1.000000 / 3.000000 is 0.333333 \n");
+    printf("The difference between %d and %d is %d \n", a, b, a-b);
+    printf("%f / %f is %f \n", f1, f2, f1/f2);
 
     return 0;
 }
